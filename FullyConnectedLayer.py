06a17bc5d37d3ed7633845e1af0373a988c16078
raobystorm <raobystorm@gmail.com>
2018-05-31 23:06:10 +0900

    good start

--- FullyConnectedLayer.py
+++ FullyConnectedLayer.py
@@ -2,14 +2,22 @@
 import numpy as np
 
 class FullyConnectedLayer:
-    def __init__(self, input, output):
-        input_image_size = input.shape[0] * input.shape[1]
-        output_image_size = output.shape[0] * output.shape[1]
-        self.weight = np.Random.random(input_image_size * output_image_size, 1, )
-        self.bias = np.Random.random()
-        self.input = np.reshape(input, (input_image_size, 1, input.shape[3], input.shape[4]))
-        self.output = np.reshape(output, (output_image_size, 1, input.shape[3], input.shape[4]))
+    # Usually the weight of fc layer is decided by the input & output's hidden units
+    def __init__(self, input, output, activation):
+        input_size = input.shape[0] * input.shape[1]
+        output_size = output.shape[0]
+        self.input = np.reshape(input, (input_size, 1,))
+        self.output = np.reshape(output, (output_size, 1,))
+        self.weight = np.random.random(input_size, output_size,)
+        self.bias = np.random.random(output_size, 1,)
+        self.delta = np.random.random(output.shape)
+        self.activation = activation
 
     def forward(self):
+        if self.activation == 'logistic':
+            self.output = 1 / (1 + np.exp(-np.dot(self.input, self.weight)))
 
     def backward(self):
+        if self.activation == 'logistic':
+            self.delta = (self.output - self.input) * (self.input * (1 - self.input))
+            self.weight += self.input.T.dot(self.delta)
